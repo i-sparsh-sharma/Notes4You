90da0df23ef03699d45725c08ec986f43e45493b
Sparsh <68753589+i-sparsh-sharma@users.noreply.github.com>
2021-07-11 17:29:16 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1 className="notehead">Javascript and React.js Bootcamp</h1>
+      <p>It is a 7 Days Basic Bootcamp on Javascript and React.js by ShapeAI</p>
+    </div>
+  );
+}
+
+export default Info;
